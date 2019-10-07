5ec9a51c7b2ddca2d436720f66d7c32e75863b7e
Filipegoncalves <filipegoncalves.ti@gmail.com>
2019-10-07 09:35:43 -0300

    chapter-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
+
+  def show
+    #@user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user,
+                  notice: 'Cadastro criado com sucesso!'
+    else
+           render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Cadastro atualizado com sucesso!'
+    else
+      render :edit
+    end
+  end
+
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:bio, :email, :full_name, :location, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :bio, :email, :full_name, :location, :password
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
-  validate_length_of :bio, minimum => 30, :allow_black => false
+  validates_presence_of :email, :full_name, :location
+  #validates_confirmation_of :password
+  validates :bio, length: { maximum: 30 }, :allow_black => false
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates_uniqueness_of :email
 
+  has_secure_password
 end
